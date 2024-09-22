4cbffbbc8dc8bf8cae303df45a0170dd23006138
Mayankjat05 <115417820+Mayankjat05@users.noreply.github.com>
2024-09-22 15:40:36 +0530

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -1,10 +1,3 @@
-# (c) @RknDeveloperr
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
-# Update Channel @Digital_Botz & @DigitalBotz_Support
-
 from aiohttp import web
 
 Rkn_FileRenameBot = web.RouteTableDef()
@@ -17,9 +10,3 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(Rkn_FileRenameBot)
     return web_app
-
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
-# Update Channel @Digital_Botz & @DigitalBotz_Support
