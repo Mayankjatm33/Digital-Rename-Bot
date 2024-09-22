1c7b22dd6a8fe75145957ba6a7d0f0f8ff7327a5
Mayankjat05 <115417820+Mayankjat05@users.noreply.github.com>
2024-09-22 15:34:46 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -110,7 +110,7 @@ async def doc(bot, update):
         suffix = await digital_botz.get_suffix(user_id)
         new_filename = add_prefix_suffix(new_filename_, prefix, suffix)
     except Exception as e:
-        return await update.message.edit(f"⚠️ Something went wrong can't able to set Prefix or Suffix ☹️ \n\n❄️ Contact My Creator -> @RknDeveloperr\nError: {e}")
+        return await update.message.edit(f"⚠️ Something went wrong can't able to set Prefix or Suffix ☹️ \n\n❄️ Contact My Creator -> @WarriorUnitsBots\nError: {e}")
 
     # msg file location 
     file = update.message.reply_to_message
