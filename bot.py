1136cd72cad5221570aea4f43628e81a591cba33
Mayankjat05 <115417820+Mayankjat05@users.noreply.github.com>
2024-09-22 15:24:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ logging.getLogger("cinemagoer").setLevel(logging.ERROR)
 class Digital_FileRenameBot(Client):
     def __init__(self):
         super().__init__(
-            name="Digital_FileRenameBot",
+            name="Warrior_FileRenameBot",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
