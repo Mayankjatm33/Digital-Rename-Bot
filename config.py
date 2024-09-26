065b7da1b49e1eb9b997d25330053e2b677afdf8
Mayankjatm33 <jatmayankjatm33@gmail.com>
2024-09-26 18:56:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ class Config(object):
     # digital_botz client config
     API_ID = os.environ.get("API_ID", "23159366")
     API_HASH = os.environ.get("API_HASH", "4623dd30dd1303bddb729eb0862262d9")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7691113005:AAF2ArAywBox7JIAsVSBHD3v7vsg1qs49Fs") 
 
     # premium account string session required 😢 
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     
     # database config
     DB_NAME = os.environ.get("DB_NAME","Warrior_Rename_Bot")     
-    DB_URL = os.environ.get("DB_URL","")
+    DB_URL = os.environ.get("DB_URL","mongodb+srv://testbot:testbot@cluster0.7rzwxwx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
     # other configs
     RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/0c3c0a72ca2785c0cf910.jpg")
